610147b64c7d9039819e35fd8f7c9361b55363d5
Blair Bonnett <blair.bonnett@hsu-hh.de>
2026-09-06 17:51:56 +0200

    Add function to downsample PDM data.

--- src/airsonar/processing/pdm_to_pressure.py
+++ src/airsonar/processing/pdm_to_pressure.py
@@ -4,6 +4,7 @@
 """Conversion of PDM signals to pressure signals."""
 
 import numpy as np
+from scipy import signal
 import xarray as xr
 
 
@@ -50,3 +51,60 @@ def binary_to_float_nrz(
         scale *= -1
 
     return scale * (data.astype(float) - offset)
+
+
+def downsample_integer(
+    data: xr.DataArray, stages: list[int], dim: str = "time"
+) -> xr.DataArray:
+    """Downsample a signal by integer factors in stages.
+
+    This uses the [scipy.signal.decimate][] function to perform each decimation. A FIR
+    anti-aliasing filter is used with an order of 20 times the factor of the stage. The
+    zero-phase option is set to avoid phase shifts when applying the filter.
+
+    Parameters
+    ----------
+    data
+        The data to downsample.
+    stages
+        A list of integer factors to downsample. A value [8, 6] downsamples first by a
+        factor of 8 and then by a factor of 6.
+    dim
+        The dimension to downsample along. The coordinates are assumed to be equally
+        spaced along this dimension.
+
+    Returns
+    -------
+    downsampled : xr.DataArray
+        The downsampled data with the same dimensions and attributes as the input data.
+
+    """
+    # scipy.signal.decimate() upcasts integers and float16. Perform the check with the
+    # same logic to ensure we tell apply_ufunc() the correct type.
+    if not np.issubdtype(data.dtype, np.inexact) or data.dtype == np.float16:
+        output_dtype = np.dtype("f8")
+    else:
+        output_dtype = data.dtype
+
+    for factor in stages:
+        # Subsample the coordinates.
+        new_coords = data[dim].values[::factor]
+
+        # And decimate the data.
+        data = xr.apply_ufunc(
+            signal.decimate,
+            data,
+            kwargs={"q": factor, "ftype": "fir", "zero_phase": True, "axis": -1},
+            input_core_dims=[[dim]],
+            output_core_dims=[[dim]],
+            exclude_dims={dim},
+            keep_attrs=True,
+            dask="parallelized",
+            output_dtypes=[output_dtype],
+            dask_gufunc_kwargs={
+                "output_sizes": {dim: len(new_coords)},
+            },
+        )
+        data = data.assign_coords({dim: new_coords})
+
+    return data
--- tests/test_pdm_to_pressure.py
+++ tests/test_pdm_to_pressure.py
@@ -60,3 +60,113 @@ def test_binary_to_float_nrz_error():
     data = xr.DataArray([1.2, 0.5, 0.4, 0.3, 1.8], coords=[("time", np.arange(5))])
     with pytest.raises(ValueError, match="only possible with binary"):
         pdm_to_pressure.binary_to_float_nrz(data)
+
+
+def to_pdm(signal: xr.DataArray) -> xr.DataArray:
+    """Generate a PDM waveform of a given signal.
+
+    This uses a fourth-order sigma-delta modulator. If the amplitude goes above ~0.7,
+    the output can become unstable.
+
+    Parameters
+    ----------
+    signal
+        The signal to generate the waveform from. This must be one-dimensional.
+
+    Returns
+    -------
+    pdm
+        The PDM waveform generated from the signal. This has the same coordinates as the
+        input signal.
+
+    """
+    if not signal.ndim == 1:
+        raise ValueError("signal must be one-dimensional")
+
+    # Initialise state.
+    integrators = np.array([0.0, 0.0, 0.0, 0.0])
+    coefficients = np.array([1, 0.5, 0.15, 0.02])
+    feedback = 0.0
+
+    # And process each sample.
+    pdm_out = np.zeros_like(signal)
+    for n in range(len(signal)):
+        integrators[0] = signal[n] - feedback
+        integrators[1] += integrators[0]
+        integrators[2] += integrators[1]
+        integrators[3] += integrators[2]
+
+        val = np.sum(integrators * coefficients)
+        feedback = 1 if val >= 0 else -1
+        pdm_out[n] = 0.5 if val >= 0 else -0.5
+
+    return xr.DataArray(pdm_out, coords=signal.coords)
+
+
+@pytest.mark.parametrize(
+    "f,fs,t_end,stages",
+    [
+        (10e3, 4.8e6, 1e-3, [8, 6]),
+        (8e3, 5e6, 1e-3, [48]),
+    ],
+)
+def test_downsample_integer_1d_sine(f, fs, t_end, stages):
+    t = np.arange(0, t_end, 1 / fs)
+    pdm = to_pdm(xr.DataArray(0.5 * np.sin(2 * np.pi * f * t), coords=[("time", t)]))
+
+    # Our PDM output is ±0.5, which halves the processed output. Also note we need to
+    # increase the allclose tolerance to handle the error from our modulator, and to
+    # skip the first few samples which have higher error.
+    downsampled = pdm_to_pressure.downsample_integer(pdm, stages)
+    expected = 0.25 * np.sin(2 * np.pi * f * downsampled["time"])
+    assert downsampled.dtype == pdm.dtype
+    xr.testing.assert_allclose(downsampled[20:], expected[20:], rtol=0, atol=5e-3)
+
+    # Check it properly upcasts.
+    pdm_int = pdm.astype("f2")
+    downsampled = pdm_to_pressure.downsample_integer(pdm_int, stages)
+    assert downsampled.dtype == float
+    xr.testing.assert_allclose(downsampled[20:], expected[20:], rtol=0, atol=5e-3)
+
+    # Check with different dimension names.
+    pdm = pdm.rename(time="sample")
+    expected = expected.rename(time="sample")
+    downsampled = pdm_to_pressure.downsample_integer(pdm, stages, dim="sample")
+    assert downsampled.dtype == pdm.dtype
+    xr.testing.assert_allclose(downsampled[20:], expected[20:], rtol=0, atol=5e-3)
+
+
+@pytest.mark.parametrize("chunked", [False, True])
+def test_downsample_integer_2d_sine(chunked):
+    # Generate multiple traces.
+    t = np.arange(0, 1e-3, 1 / 4.8e6)
+    traces = []
+    for p in range(7):
+        traces.append(
+            to_pdm(
+                xr.DataArray(
+                    0.5 * np.sin(2 * np.pi * p * 1e3 * t), coords=[("time", t)]
+                )
+            )
+        )
+    pdm = xr.concat(traces, "ping").assign_coords(ping=np.arange(7))
+
+    # Downsample.
+    if chunked:
+        downsampled = pdm_to_pressure.downsample_integer(pdm.chunk(ping=3), [8, 6])
+        assert downsampled.chunksizes == {"ping": (3, 3, 1), "time": (len(t) // 48,)}
+        downsampled.load()
+    else:
+        downsampled = pdm_to_pressure.downsample_integer(pdm, [8, 6])
+    assert not downsampled.chunksizes
+
+    # Our PDM output is ±0.5, which halves the processed output. Also note we need to
+    # increase the allclose tolerance to handle the error from our modulator, and to
+    # skip the first few samples which have higher error.
+    expected_traces: list[xr.DataArray] = []
+    for p in range(7):
+        expected_traces.append(0.25 * np.sin(2 * np.pi * p * 1e3 * downsampled["time"]))  # type:ignore[arg-type]
+    expected = xr.concat(expected_traces, "ping").assign_coords(ping=np.arange(7))
+    xr.testing.assert_allclose(
+        downsampled[20:], expected[20:], rtol=0, atol=5e-3, check_dim_order=False
+    )
