89ae4f331743367a5a42499afd982a49c02aefef
Blair Bonnett <blair.bonnett@hsu-hh.de>
2026-09-10 16:55:45 +0200

    Docs: use KaTeX for rendering math.

---
+++ docs/javascript/katex.js
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: AirSonar contributors
+// SPDX-License-Identifier: CC0-1.0
+
+document$.subscribe(({ body }) => {
+  renderMathInElement(body, {
+    delimiters: [
+      { left: "$$",  right: "$$",  display: true },
+      { left: "$",   right: "$",   display: false },
+      { left: "\\(", right: "\\)", display: false },
+      { left: "\\[", right: "\\]", display: true }
+    ],
+  })
+})
